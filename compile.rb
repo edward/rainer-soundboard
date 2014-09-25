e0f1b25be9381c9a2b774b50bfb09ae8ec38955f
Edward Ocampo-Gooding <edward@edwardog.net>
2014-09-24 23:36:24 -0400

    Such hacks

--- compile.rb
+++ compile.rb
@@ -1,10 +1,20 @@
 require "erb"
 
 sound_file_names = Dir.glob("sounds/*.{mp3, ogg}")
+
+sound_ids = sound_file_names.map do |sound|
+  sound.gsub("sounds/", '').gsub(/\..+$/, '').gsub(/\W/, '')
+end
+
+def button_id(sound_id)
+  sound_id + "-button"
+end
+
+def sound_id(sound)
+  sound.gsub("sounds/", '').gsub(/\..+$/, '').gsub(/\W/, '')
+end
+
 template = File.read("index.html.erb")
-
-herp = "yep"
-
 result = ERB.new(template).result
 
 File.open("index.html", "w+") do |f|
